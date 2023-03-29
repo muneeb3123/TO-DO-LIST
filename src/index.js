583d166dcd16d99156f8553ffc02937c7b3a2ba2
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-03-30 01:37:28 +0500

    nnnm

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
-import './style.css';
+import './styles.css';
 
-const tasks = [
-    {
-      description: 'Task 1',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'Task 2',
-      completed: true,
-      index: 2,
-    },
-    {
-      description: 'Task 3',
-      completed: false,
-      index: 3,
-    },
-  ];
+const tasksArr = [
+  {
+    description: 'Write code',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Watch football',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Sleep',
+    completed: true,
+    index: 2,
+  },
+];
 
-  const renderTasks = () => {
-    const toDoList = document.getElementById('to-do-list');
-    toDoList.innerHTML = '';
-    tasks.sort((a, b) => a.index - b.index);
-    const taskItems = tasks.map((task) => {
-      const taskItem = document.createElement('li');
-      taskItem.innerText = task.description;
-      if (task.completed) {
-        taskItem.classList.add('completed');
-      }
-      return taskItem;
-    });
-    toDoList.append(...taskItems);
-  };
+const toDoList = document.getElementById('to-do-list');
 
-  renderTasks();
+function populateTaskList(arr) {
+  arr.forEach((task) => {
+    const toDoItem = document.createElement('li');
+    toDoItem.classList.add('to-do-item');
+    toDoItem.innerHTML = `
+    <button class="check-button">
+      <span class="fas fa-check"></span>
+    </button>
+    <span class="task-description">${task.description}</span>
+    <span class="fas fa-ellipsis-v"></span>
+    `;
+    toDoList.appendChild(toDoItem);
+  });
+}
+
+populateTaskList(tasksArr);
