e6d46c697cdb5fcf769ada66d4c7a09f5dea9b9d
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-03-30 01:30:39 +0500

    hjkklk

--- src/index.js
+++ src/index.js
@@ -1,38 +1,69 @@
-import './styles.css';
+import { currentTime } from './modules/showtime.js';
+import { addClick, contactClick, listClick } from './modules/operations.js';
+import { variables } from './modules/link.js';
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+class showBook {
+    constructor() {
+        this.bookList = variables.booklist;
+        this.addBtn = variables.addBtn;
+        this.collection = JSON.parse(localStorage.getItem('bookCollection')) || [];
+        this.addBtn.addEventListener('click', () => {
+            this.addBook();
+        });
+        this.renderCollection();
 
-const tasksArr = [
-  {
-    description: 'Write code',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Watch football',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Sleep',
-    completed: true,
-    index: 2,
-  },
-];
+    }
 
-const toDoList = document.getElementById('to-do-list');
-
-function populateTaskList(arr) {
-  arr.forEach((task) => {
-    const toDoItem = document.createElement('li');
-    toDoItem.classList.add('to-do-item');
-    toDoItem.innerHTML = `
-    <button class="check-button">
-      <span class="fas fa-check"></span>
-    </button>
-    <span class="task-description">${task.description}</span>
-    <span class="fas fa-ellipsis-v"></span>
-    `;
-    toDoList.appendChild(toDoItem);
-  });
+    renderCollection() {
+        let html = '';
+        this.collection.forEach((element, index) => {
+            html += `<div class='book'>
+            <span class=" white lists-header">
+             <p class="title-header bold">${element.title}</p>
+             <p class="author-header bold">${element.author}</p>
+             <button class="RemoveButton">Remove</button>
+         </span>
+                 </div>`;
+        });
+        this.bookList.innerHTML = html;
+        this.removeBtns = document.querySelectorAll('.RemoveButton');
+        this.removeBtns.forEach((element, index) => {
+            element.addEventListener('click', () => {
+                this.removeBook(element, index);
+            });
+        });
+    }
+    addBook() {
+        if (variables.Title.value && variables.Author.value) {
+            this.book = new Book(variables.Title.value, variables.Author.value);
+            this.collection.push(this.book);
+            localStorage.setItem('bookCollection', JSON.stringify(this.collection));
+            this.renderCollection();
+            variables.Title.value = '';
+            variables.Author.value = '';
+        }
+    }
+    removeBook(element, bookIndex) {
+        this.collection = this.collection.filter((element, index) => index !== bookIndex);
+        localStorage.setItem('bookCollection', JSON.stringify(this.collection));
+        this.renderCollection();
+    }
 }
 
-populateTaskList(tasksArr);
+//NavBar Links
+
+variables.ListLink.addEventListener('click', listClick)
+
+variables.AddLink.addEventListener('click', addClick)
+
+variables.ContactLink.addEventListener('click', contactClick)
+
+
+//Show Time
+const runTime = new showBook();
+setInterval(currentTime, 1000);
