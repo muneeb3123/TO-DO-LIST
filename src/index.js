48f0446aad798b14d4856ec085c7c99e31e6c209
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-03-30 01:02:26 +0500

    fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import './style.css';
 
-import renderTasks from '../modules/render.js';
+const tasks = [
+    {
+      description: 'Task 1',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Task 2',
+      completed: true,
+      index: 2,
+    },
+    {
+      description: 'Task 3',
+      completed: false,
+      index: 3,
+    },
+  ];
 
-renderTasks();
+  const renderTasks = () => {
+    const toDoList = document.getElementById('to-do-list');
+    toDoList.innerHTML = '';
+    tasks.sort((a, b) => a.index - b.index);
+    const taskItems = tasks.map((task) => {
+      const taskItem = document.createElement('li');
+      taskItem.innerText = task.description;
+      if (task.completed) {
+        taskItem.classList.add('completed');
+      }
+      return taskItem;
+    });
+    toDoList.append(...taskItems);
+  };
+
+  renderTasks();
