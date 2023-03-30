51a5cfc6f8adf06eb537c0cb24a613f56c24c114
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-03-30 06:35:31 +0500

    add template

--- modules/render.js
+++ modules/render.js
@@ -4,15 +4,18 @@ const renderTasks = () => {
   const toDoList = document.getElementById('to-do-list');
   toDoList.innerHTML = '';
   tasks.sort((a, b) => a.index - b.index);
-  const taskItems = tasks.map((task) => {
-    const taskItem = document.createElement('li');
-    taskItem.innerText = task.description;
-    if (task.completed) {
-      taskItem.classList.add('completed');
-    }
-    return taskItem;
-  });
-  toDoList.append(...taskItems);
+  toDoList.innerHTML = tasks
+    .map(
+      (task) => `
+    <li class='to-do-item'>
+      <button class='check-button'>
+      </button>
+      <span class='task-description'>${task.description}</span>
+      <span class='fas fa-ellipsis-v'></span>
+    </li>
+  `,
+    )
+    .join('');
 };
 
 export default renderTasks;
