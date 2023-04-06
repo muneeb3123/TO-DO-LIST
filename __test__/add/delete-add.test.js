398aa940d4616ac41e5ce579092f2c8f32020145
Daniela Moreno <danielamoreno699@gmail.com>
2023-04-06 12:17:57 -0500

    Add local storage function

--- __test__/add/delete-add.test.js
+++ __test__/add/delete-add.test.js
@@ -5,18 +5,27 @@ describe('ToDoList', () => {
   let todoItem;
   const Taskdescription = 'something';
 
-  beforeAll(() => {
-    document.body.innerHTML = `
-    <form class="add-submit">
-    <input placeholder="Add your task..." type="text">
-    <input type="submit" class="submit" value="" title="click this or press enter to submit">
-</form>
-    <ul id="to-do-list"></ul>
-    <button class="clear-completed">Clear Completed</button>
-  `;
-  });
-
   beforeEach(() => {
+    // mocking localStorage
+    Object.defineProperty(window, 'localStorage', {
+      value: {
+        getItem: jest.fn(() => JSON.stringify([{ description: 'something', completed: false, index: 1 }])),
+        setItem: jest.fn(() => null),
+        clear: jest.fn(() => null),
+      },
+      writable: true,
+    });
+
+    document.body.innerHTML = `
+      <form class="add-submit">
+        <input placeholder="Add your task..." type="text">
+        <input type="submit" class="submit" value="" title="click this or press enter to submit">
+      </form>
+      <ul id="to-do-list"></ul>
+      <button class="clear-completed">Clear Completed</button>
+    `;
+
+    // create a new ToDoList instance
     todoItem = new ToDoList();
   });
 
@@ -28,12 +37,16 @@ describe('ToDoList', () => {
       completed: false,
       index: expect.any(Number),
     });
+
     expect(todoItem.collection.length).toBeGreaterThan(0);
+    expect(window.localStorage.setItem).toHaveBeenCalledWith('taskCollection', JSON.stringify(todoItem.collection));
   });
 
   test('remove item from list', () => {
     todoItem.removeBook(0);
+
     expect(todoItem.collection).toEqual([]);
     expect(todoItem.removeBook(0)).toBeFalsy();
+    expect(window.localStorage.setItem).toHaveBeenCalledWith('taskCollection', JSON.stringify(todoItem.collection));
   });
 });
