6c8f6468b4b035b26722cb9b2443b041eb82baf0
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-04-06 22:50:52 +0500

    completed

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,5 +1,4 @@
-import { form, input } from './link.js';
-
+import { form,input } from "./link";
 export class ToDoList {
   constructor() {
     this.collection = JSON.parse(localStorage.getItem('taskCollection')) || [];
